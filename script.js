172aa6accc1ff60cfb38658bdf30bf8d2a0fedfe
Seenemees <68758659+Seenemees@users.noreply.github.com>
2023-08-10 09:10:28 +0300

    Create playRound function

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+
+
+
 let getComputerChoice = () => {
-    let choices = ["Rock", "Paper", "Scissor"];
+    let choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
 
 }
+
+let playRound = (playerSelection, computerSelection) => {
+    computerSelection = getComputerChoice();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    let won = `You won! ${playerSelection} beats ${computerSelection}!`;
+    let lost = `You lost! ${playerSelection} loses to ${computerSelection}!`;
+    let tied = `You tied! ${playerSelection} cancels out ${computerSelection}!`;
+    if (computerSelection === playerSelection) {
+        return tied;
+    } else if ((playerSelection === "Rock" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Rock")) {
+        return lost;
+    } else {
+        return won;
+    }
+}
+
